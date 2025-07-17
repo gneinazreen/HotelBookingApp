be96d0a7e6d9e9353ec0938e23d1aa42bb379c7c
gneinaz123 <gneinazreen3@gmail.com>
2025-07-17 16:16:50 +0530

    Chatbotform

--- HotelBookingApp/Views/ChatbotForm.Designer.cs
+++ HotelBookingApp/Views/ChatbotForm.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtResponse = new System.Windows.Forms.RichTextBox();
+            this.btnAsk = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtQuestion = new System.Windows.Forms.RichTextBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(0, 69);
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(294, 294);
+            this.txtResponse.TabIndex = 1;
+            this.txtResponse.Text = "";
+            // 
+            // btnAsk
+            // 
+            this.btnAsk.Location = new System.Drawing.Point(251, 232);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(75, 23);
+            this.btnAsk.TabIndex = 2;
+            this.btnAsk.Text = "Ask";
+            this.btnAsk.UseVisualStyleBackColor = true;
+            this.btnAsk.Click += new System.EventHandler(this.btnAsk_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtResponse);
+            this.panel1.Location = new System.Drawing.Point(332, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(297, 363);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblQuestion);
+            this.panel2.Controls.Add(this.txtQuestion);
+            this.panel2.Location = new System.Drawing.Point(0, 99);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(245, 363);
+            this.panel2.TabIndex = 4;
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(0, 69);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(242, 291);
+            this.txtQuestion.TabIndex = 0;
+            this.txtQuestion.Text = "";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(78, 34);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(72, 13);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Ask Anything!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Response Recieved";
+            // 
+            // ChatbotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(630, 464);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAsk);
+            this.Name = "ChatbotForm";
             this.Text = "ChatbotForm";
+            this.Load += new System.EventHandler(this.ChatbotForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtResponse;
+        private System.Windows.Forms.Button btnAsk;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox txtQuestion;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label1;
     }
 }
--- HotelBookingApp/Views/ChatbotForm.cs
+++ HotelBookingApp/Views/ChatbotForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelBookingApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,57 @@ namespace HotelBookingApp.Views
         public ChatbotForm()
         {
             InitializeComponent();
+        }
+
+        private void ChatbotForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAsk_Click(object sender, EventArgs e)
+        {
+            string question = txtQuestion.Text.ToLower();
+            string response = GetChatbotResponse(question);
+            txtResponse.Text = response;
+
+        }
+        private string GetChatbotResponse(string question)
+        {
+            var bookings = DataStorage.Bookings;
+            var rooms = DataStorage.Rooms;
+
+            if(question.Contains("available") && question.Contains("single"))
+            {
+                var futureDate = DateTime.Today.AddDays(7);
+                int booked = bookings.Count(b => b.RoomId == 1 && b.CheckInDate <= futureDate && b.CheckOutDate > futureDate);
+                return booked < 5 ? "Single rooms will be available next week." : "No Single room available for next week";
+
+            }
+            if (question.Contains("price") && question.Contains("double"))
+            {
+                var basePrice = rooms.FirstOrDefault(r => r.RoomType.ToLower() == "double")?.BasePrice ?? 100;
+                int doubleBookings = bookings.Count(b => b.RoomId == 2);
+                if (doubleBookings > 10)
+                {
+                    return $"Expected price: {basePrice + 20} (high demand)";
+                }
+                else
+                {
+                    return $"Expected price: {basePrice} (normal demand)";
+                }
+            }
+            if (question.Contains("busiest day"))
+            {
+                var busiestDay = bookings
+                    .SelectMany(b => Enumerable.Range(0, (b.CheckOutDate - b.CheckInDate).Days)
+                    .Select(offset => b.CheckInDate.AddDays(offset).DayOfWeek))
+                    .GroupBy(d => d)
+                    .OrderByDescending(g => g.Count())
+                    .FirstOrDefault()?.Key;
+
+                return $"The busiest day is likely: {busiestDay}";
+            }
+            return "Try Asking about availability, price or buisest day.";
         }
     }
 }
