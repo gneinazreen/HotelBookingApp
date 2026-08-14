a26b9fe2c0c80a08254cc25cb30487308de2a189
Nazreen <nazreen@gmail.com>
2026-08-14 10:04:40 +0530

    Dashboard UI Design

--- HotelBookingApp/Views/Main.Designer.cs
+++ HotelBookingApp/Views/Main.Designer.cs
@@ -39,69 +39,83 @@
             // btnBookings
             // 
             this.btnBookings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.btnBookings.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnBookings.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBookings.ForeColor = System.Drawing.Color.White;
             this.btnBookings.Location = new System.Drawing.Point(49, 138);
-            this.btnBookings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBookings.Margin = new System.Windows.Forms.Padding(4);
             this.btnBookings.Name = "btnBookings";
-            this.btnBookings.Size = new System.Drawing.Size(155, 55);
+            this.btnBookings.Size = new System.Drawing.Size(237, 55);
             this.btnBookings.TabIndex = 0;
             this.btnBookings.Text = "Manage Bookings";
+            this.btnBookings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.btnBookings.UseVisualStyleBackColor = false;
             this.btnBookings.Click += new System.EventHandler(this.btnBookings_Click);
             // 
             // btnRooms
             // 
-            this.btnRooms.Location = new System.Drawing.Point(248, 138);
-            this.btnRooms.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRooms.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnRooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRooms.ForeColor = System.Drawing.Color.White;
+            this.btnRooms.Location = new System.Drawing.Point(305, 138);
+            this.btnRooms.Margin = new System.Windows.Forms.Padding(4);
             this.btnRooms.Name = "btnRooms";
-            this.btnRooms.Size = new System.Drawing.Size(128, 55);
+            this.btnRooms.Size = new System.Drawing.Size(181, 55);
             this.btnRooms.TabIndex = 1;
             this.btnRooms.Text = "Manage Rooms";
-            this.btnRooms.UseVisualStyleBackColor = true;
+            this.btnRooms.UseVisualStyleBackColor = false;
             this.btnRooms.Click += new System.EventHandler(this.btnRooms_Click);
             // 
             // btnRequests
             // 
-            this.btnRequests.Location = new System.Drawing.Point(405, 138);
-            this.btnRequests.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRequests.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnRequests.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRequests.ForeColor = System.Drawing.Color.White;
+            this.btnRequests.Location = new System.Drawing.Point(507, 138);
+            this.btnRequests.Margin = new System.Windows.Forms.Padding(4);
             this.btnRequests.Name = "btnRequests";
-            this.btnRequests.Size = new System.Drawing.Size(157, 58);
+            this.btnRequests.Size = new System.Drawing.Size(211, 58);
             this.btnRequests.TabIndex = 2;
             this.btnRequests.Text = "Manage Requests";
-            this.btnRequests.UseVisualStyleBackColor = true;
+            this.btnRequests.UseVisualStyleBackColor = false;
             this.btnRequests.Click += new System.EventHandler(this.btnRequests_Click);
             // 
             // btnReports
             // 
-            this.btnReports.Location = new System.Drawing.Point(145, 222);
-            this.btnReports.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReports.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.White;
+            this.btnReports.Location = new System.Drawing.Point(139, 230);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(4);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(141, 50);
+            this.btnReports.Size = new System.Drawing.Size(203, 50);
             this.btnReports.TabIndex = 2;
             this.btnReports.Text = "Weekly Report";
-            this.btnReports.UseVisualStyleBackColor = true;
+            this.btnReports.UseVisualStyleBackColor = false;
             this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
             // 
             // btnChatbot
             // 
-            this.btnChatbot.Location = new System.Drawing.Point(321, 222);
-            this.btnChatbot.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnChatbot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnChatbot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChatbot.ForeColor = System.Drawing.Color.White;
+            this.btnChatbot.Location = new System.Drawing.Point(369, 230);
+            this.btnChatbot.Margin = new System.Windows.Forms.Padding(4);
             this.btnChatbot.Name = "btnChatbot";
             this.btnChatbot.Size = new System.Drawing.Size(133, 50);
             this.btnChatbot.TabIndex = 2;
             this.btnChatbot.Text = "Chatbot";
-            this.btnChatbot.UseVisualStyleBackColor = true;
+            this.btnChatbot.UseVisualStyleBackColor = false;
             this.btnChatbot.Click += new System.EventHandler(this.btnChatbot_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.AliceBlue;
-            this.label4.Location = new System.Drawing.Point(188, 70);
+            this.label4.Location = new System.Drawing.Point(233, 69);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(249, 29);
+            this.label4.Size = new System.Drawing.Size(290, 29);
             this.label4.TabIndex = 17;
             this.label4.Text = "My Hotel Dashboard";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -110,15 +124,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.ClientSize = new System.Drawing.Size(755, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(755, 380);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnChatbot);
             this.Controls.Add(this.btnReports);
             this.Controls.Add(this.btnRequests);
             this.Controls.Add(this.btnRooms);
             this.Controls.Add(this.btnBookings);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Main";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
